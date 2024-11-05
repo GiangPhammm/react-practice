acef1e49971707829540283eaed799d018398301
Giang Pham <phamthugiang2604@gmail.com>
2024-11-05 13:37:17 +0100

    footer

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 
 import {Header} from './components/header';
 import {NewList} from './components/news-list';
+import {Footer} from './components/footer';
 import './styles/style.css';
 import JSON from './db.json';
 
 const App = () => {
+    const initialState = {
+        news: JSON,
+        footerText: 'I am a happy footer'
+    }
     
     return (
         <>
             <Header />
             <NewList
-                news={JSON}
+                news={initialState.news}
             />
+            <Footer footerText={initialState.footerText} />
         </>
     );
 }
