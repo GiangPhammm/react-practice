d9e2141471f65930a3421168450d820347b09c13
Giang Pham <phamthugiang2604@gmail.com>
2024-11-12 20:19:15 +0100

    different functions in lifecycle

--- src/components/lifecycle.jsx
+++ src/components/lifecycle.jsx
@@ -6,31 +6,65 @@ export class Life extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {action: ''};
-        console.log('1-contructor')
+        this.state = {name: 'Francis'};
+        console.log('1-contructor');
     }
 
     // first real lifecycle on a component within react that kicks in
-    static getDerivedStateFromProps() {
-        console.log('2-getDerivedStateFromProps')
+    // get access to current state, and props
+    static getDerivedStateFromProps(props, state) {
+        console.log('2-getDerivedStateFromProps');
+
+        // intercept the state
+        if (state.name === 'Ron') {
+            return {
+                name: 'Milhouse'
+            }
+        }
         return null;
     }
 
     // everything got mounted after the render is visible
     componentDidMount() {
-        console.log('4-componentDidMount')
+        console.log('4-componentDidMount');
     }
 
     // listen when react remove components from screen
     componentWillUnmount() {
-        console.log('5-componentWillUnmount')
+        console.log('5-componentWillUnmount');
     }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(nextState.name === 'Steve'){
+            return false
+        }
+
+        return true;
+    }
+
+    // store snapshots of something and pass along to other lifecycle
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        let age = 20;
+
+        return age;
+    }
+
+    // trigger if the component was updated
+    componentDidUpdate(prevProps, prevState, snapshots) {
+        // console.log(prevState);
+        // console.log(this.state);
+        console.log(snapshots)
+    }
+
 
     render() {
         console.log('3-render')
         return (
             <div>
-                LIFE
+                <div>{this.state.name}</div>
+                <div onClick={() => this.setState({name: 'Ron'})}>
+                    change name
+                </div>
             </div>
         )
     }
